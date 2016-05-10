4656712103037a91cb813fc2e51b565da9e3e3bf
VietNH <vietpiano@gmail.com>
2016-05-10 17:10:25 +0700

    VietNH create ums_api phalcon

--- ums_api/api/app/config/config.php
+++ ums_api/api/app/config/config.php
@@ -6,7 +6,7 @@ defined('APP_PATH') || define('APP_PATH', realpath('.'));
 return new \Phalcon\Config(array(
     'database' => array(
         'adapter'     => 'Mysql',
-        'host'        => 'localhost',
+        'host'        => '10.0.0.19',
         'username'    => 'root',
         'password'    => '123456789$$',
         'dbname'      => 'usermanagement',
