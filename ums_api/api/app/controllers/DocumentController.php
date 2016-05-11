60efbd5918ad7b082a340dcdaa031d2b6ede875c
VietNH <vietpiano@gmail.com>
2016-05-11 14:22:05 +0700

    Upgrade code with comment

--- ums_api/api/app/controllers/DocumentController.php
+++ ums_api/api/app/controllers/DocumentController.php
@@ -2,5 +2,12 @@
 
 class DocumentController extends BaseController
 {
-
+    public function initialize()
+    {
+        parent::initialize();
+        $this->view->setMainView("documentation");
+    }
+    public function indexAction(){
+        
+    }
 }
