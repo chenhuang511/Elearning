6d7eca5ca0c3e9f741b5787433be741bc04a4afc
hanv <vanhacnttbk@gmail.com>
2016-05-12 09:36:49 +0700

    update.volt, detail.voil update not show pass

--- ums_api/api/app/controllers/UserController.php
+++ ums_api/api/app/controllers/UserController.php
@@ -81,6 +81,7 @@ class UserController extends RESTController
             }
             else{
                 $userobject = User::findFirst(array(
+                    "columns"=>"id,firstname,lastname,username,address,email",
                     "conditions"=>"id = :userid:",
                     "bind"=>array("userid"=>$userid)
                 ));
@@ -148,7 +149,11 @@ class UserController extends RESTController
                 if($flag_update==true){
                     $userobject->map_object($datapost); // Đồng bộ hóa form với các thông tin cột trong userobject
                     $userobject->save(); // Lưu vào Database
-                    $userobject = User::findFirst($userid); // Select ngược lại thông tin để lấy chính xác thông tin user trả về cho client
+                    $userobject = User::findFirst(array(// Select ngược lại thông tin để lấy chính xác thông tin user trả về cho client
+                        "columns"=>"id,firstname,lastname,username,address,email",
+                        "conditions"=>"id = :userid:",
+                        "bind"=>array("userid"=>$userid)
+                    ));
                     $mss = "Successfully";
                     $status = 1;
                 }
--- ums_api/api/app/views/document/detail_user_by_id.volt
+++ ums_api/api/app/views/document/detail_user_by_id.volt
@@ -16,37 +16,16 @@
 <div>
 <pre class="prettyprint">
 {
-    "status": 1,
-    "mss": "Successfully",
-    "data": {
-        "id": "1",
-        "username": "admin",
-        "password": "6a0d8f756dfa9c1cd69fed28423ec8ca",
-        "firstname": "Nguyễn Hoàng",
-        "lastname": "Việt",
-        "type": "1",
-        "avatar": "uploads/2016/03/16//picture/f3a369bb43b7c13bcc7693250c6c40c5_desert.jpg",
-        "dob": "0",
-        "email": "vietpiano@gmail.com",
-        "address": "Hà Nội",
-        "phone": "0123456789",
-        "datecreate": "1462961632",
-        "usercreate": "1",
-        "gender": "1",
-        "private_permission": null,
-        "flags": "system",
-        "classid": null,
-        "activekey": null,
-        "fbid": "123456",
-        "fbemail": "",
-        "status": null,
-        "active_register": null,
-        "phone2": null,
-        "father_name": null,
-        "mother_name": null,
-        "captions": null,
-        "pos_id": null
-    }
+  status: 1,
+  mss: "Successfully",
+  data: {
+    id: "5",
+    firstname: "Nguyễn Thế",
+    lastname: "Cơ",
+    username: "supercom",
+    address: "164 Khuất Duy Tiến",
+    email: "nhacdjchamvn@gmail.com"
+  }
 }
 </pre>
 </div>
