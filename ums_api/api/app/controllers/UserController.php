f090d184032a3bffd31bb39744be7fba9f630912
hanv <vanhacnttbk@gmail.com>
2016-05-12 08:33:21 +0700

    HaNV update user api

--- ums_api/api/app/controllers/UserController.php
+++ ums_api/api/app/controllers/UserController.php
@@ -75,20 +75,90 @@ class UserController extends RESTController
      */
     public function detailAction(){
         try{
-            // Xử lý form
-            $userid = $this->request->get("userid","int");
-            $mss = "";
-            $userobject = User::findFirst(array(
-                "conditions"=>"id = :userid:",
-                "bind"=>array("userid"=>$userid)
-            ))->toArray();
-            $this->datarespone = array("status"=>1,"mss"=>"Successfully","data"=>$userobject);
-            $this->setPayload($this->datarespone);
+            $userid = $this->request->get("userid","int"); // Get userid from request
+            if(!$this->session->has("uinfo")){
+                $this->datarespone = array("status"=>0,"mss"=>"User not authenticate","data"=>new stdClass());
+            }
+            else{
+                $userobject = User::findFirst(array(
+                    "conditions"=>"id = :userid:",
+                    "bind"=>array("userid"=>$userid)
+                ));
+                if($userobject->id<=0){
+                    $this->datarespone = array("status"=>0,"mss"=>"User not available","data"=>new stdClass());
+                }
+                else{
+                    $this->datarespone = array("status"=>1,"mss"=>"Successfully","data"=>$userobject->toArray());
+                }
+            }
         }
         catch (Exception $e){
-
+            $this->datarespone = array("status"=>0,"mss"=>$e->getMessage(),"data"=>new stdClass());
         }
+        $this->setPayload($this->datarespone);
         $this->render();
     }
 
+    /***
+     * Update User profile - Sửa dữ liệu user của chính người dùng đó
+     * @param password,email,firstname,lastname,dob,address,phone,gender (can't update username)
+     * @return UserObject(userid)
+     */
+    public function updateAction(){
+        try{
+            // Nếu người dùng chưa đăng nhập -> báo lỗi
+            if(!$this->session->has("uinfo")){
+                $this->datarespone = array("status"=>0,"mss"=>"User not authenticate","data"=>new stdClass());
+            }
+            else{
+                $userinfo = $this->session->get("uinfo");
+                $userid = $userinfo['id'];
+                $datapost = Helper::post_to_array("password,email, firstname, lastname, dob, address, phone, gender"); // Get data from request
+                // Xử lý với datapost nhận được
+                $userobject = User::findFirst($userid);
+                // Với mỗi dữ liệu nhận được, nếu dl==null -> unset datapost trường dl đó
+                foreach($datapost as $key => $value){
+                    if(strlen($value)>0){
+                        if($key = 'password'){
+                            $datapost['password'] = Helper::encryptpassword($datapost['password']);
+                        }
+                    }
+                    else{
+                        unset($datapost[$key]);
+                    }
+                }
+                $datapost['dob'] = strtotime($datapost['dob']);
+                $flag_update = true;
+                $status = $mss = "";
+                // Validation email
+                if (strlen($datapost['email']) >0 && $datapost['email'] != $userobject->email ) { // Nếu datapost['email'] khác với email cũ trong db -> update email
+                    if (strlen($datapost['email']) > 0) { // Kiểm tra xem email đã tồn tại trên hệ thống hay chưa
+                        $c = User::count(array(
+                            "conditions" => "email = :email:",
+                            "bind" => array("email" => $datapost['email'])
+                        ));
+                        if($c>0) {
+                            $flag_update = false;
+                            $mss = "Email duplicated";
+                            $status = 0;
+                        }
+                    }
+                }
+
+                if($flag_update==true){
+                    $userobject->map_object($datapost); // Đồng bộ hóa form với các thông tin cột trong userobject
+                    foreach($datapost as $key=>$item) if(strlen($item)<=0) unset($userobject->{$key});
+                    $userobject->save(); // Lưu vào Database
+                    $userobject = User::findFirst($userid); // Select ngược lại thông tin để lấy chính xác thông tin user trả về cho client
+                }
+
+                $this->datarespone = array("status"=>$status,"mss"=>"$mss","data"=>$userobject->toArray());
+            }
+        }
+        catch (Exception $e){
+            $this->datarespone = array("status"=>0,"mss"=>$e->getMessage(),"data"=>new stdClass());
+        }
+        $this->setPayload($this->datarespone);
+        $this->render();
+    }
 }
