a83cb8bd3d83244c3e1050ac7279478d1703247e
hanv <vanhacnttbk@gmail.com>
2016-05-23 14:53:07 +0700

    update moodle/config test

--- moodle/config.php
+++ moodle/config.php
@@ -4,75 +4,6 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-<<<<<<< HEAD
-$CFG->dbtype    = 'mysqli';      // 'pgsql', 'mariadb', 'mysqli', 'mssql', 'sqlsrv' or 'oci'
-$CFG->dblibrary = 'native';     // 'native' only at the moment
-$CFG->dbhost    = 'localhost';  // eg 'localhost' or 'db.isp.com' or IP
-$CFG->dbname    = 'moodle';     // database name, eg moodle
-$CFG->dbuser    = 'root';   // your database username
-$CFG->dbpass    = '123456$';   // your database password
-$CFG->prefix    = 'mdl_';       // prefix to use for all table names
-$CFG->dboptions = array(
-    'dbpersist' => false,       // should persistent database connections be
-                                //  used? set to 'false' for the most stable
-                                //  setting, 'true' can improve performance
-                                //  sometimes
-    'dbsocket'  => false,       // should connection via UNIX socket be used?
-                                //  if you set it to 'true' or custom path
-                                //  here set dbhost to 'localhost',
-                                //  (please note mysql is always using socket
-                                //  if dbhost is 'localhost' - if you need
-                                //  local port connection use '127.0.0.1')
-    'dbport'    => '',          // the TCP port number to use when connecting
-                                //  to the server. keep empty string for the
-                                //  default port
-);
-
-$CFG->opensslcnf = 'D:\Project\Teca_pro\openssl.cnf';
-
-//=========================================================================
-// 2. WEB SITE LOCATION
-//=========================================================================
-// Now you need to tell Moodle where it is located. Specify the full
-// web address to where moodle has been installed.  If your web site
-// is accessible via multiple URLs then choose the most natural one
-// that your students would use.  Do not include a trailing slash
-//
-// If you need both intranet and Internet access please read
-// http://docs.moodle.org/en/masquerading
-
-$CFG->wwwroot   = 'http://10.0.0.254:1111';
-
-
-//=========================================================================
-// 3. DATA FILES LOCATION
-//=========================================================================
-// Now you need a place where Moodle can save uploaded files.  This
-// directory should be readable AND WRITEABLE by the web server user
-// (usually 'nobody' or 'apache'), but it should not be accessible
-// directly via the web.
-//
-// - On hosting systems you might need to make sure that your "group" has
-//   no permissions at all, but that "others" have full permissions.
-//
-// - On Windows systems you might specify something like 'c:\moodledata'
-
-$CFG->dataroot  = 'D:\Project\Teca_pro';
-
-
-//=========================================================================
-// 4. DATA FILES PERMISSIONS
-//=========================================================================
-// The following parameter sets the permissions of new directories
-// created by Moodle within the data directory.  The format is in
-// octal format (as used by the Unix utility chmod, for example).
-// The default is usually OK, but you may want to change it to 0750
-// if you are concerned about world-access to the files (you will need
-// to make sure the web server process (eg Apache) can access the files.
-// NOTE: the prefixed 0 is important, and don't use quotes.
-
-$CFG->directorypermissions = 02777;
-=======
 $CFG->dbtype    = 'mysqli';
 $CFG->dblibrary = 'native';
 $CFG->dbhost    = 'localhost';
@@ -82,15 +13,14 @@ $CFG->dbpass    = '12345';
 
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
-  'dbpersist' => 0,
-  'dbport' => '',
-  'dbsocket' => '',
+    'dbpersist' => 0,
+    'dbport' => '',
+    'dbsocket' => '',
 );
 
 $CFG->wwwroot   = 'http://10.0.0.85/moodle';
 $CFG->dataroot  = 'C:\\wamp\\moodledata';
 $CFG->admin     = 'admin';
->>>>>>> origin/master
 
 $CFG->directorypermissions = 0777;
 
@@ -99,7 +29,7 @@ $CFG->logoutredir = "{$CFG->wwwroot}";
 
 $CFG->opensslcnf = 'C:\\wamp\\bin\\apache\\apache2.4.9\\conf\\openssl.cnf';
 
-$CFG->debug = 6143; 
+$CFG->debug = 6143;
 $CFG->debugdisplay = 1;
 
 require_once(dirname(__FILE__) . '/lib/setup.php');
