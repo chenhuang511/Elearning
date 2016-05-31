721a4c2a44d558d6df8c7c484d0fb39361e1ba62
VietNH <vietpiano@gmail.com>
2016-05-31 11:21:26 +0700

    Merge branch 'master' of D:\Project\Teca_pro\User_mangement\Elearning with conflicts.

--- moodle/mod/assign/remote/remotelib.php
+++ moodle/mod/assign/remote/remotelib.php
@@ -2,12 +2,24 @@
 defined('MOODLE_INTERNAL') || die;
 
 require_once($CFG->dirroot . '/lib/remote/lib.php');
-require_once($CFG->dirroot . '/lib/additionallib.php');
-require_once($CFG->dirroot . '/mnet/service/enrol/locallib.php');
-function get_assign_summary_remote($courseid, $options = []){
-    return moodle_webservice_client(array_merge($options,array('domain' => HUB_URL,
-        'token' => HOST_TOKEN,
+function get_assign_summary_remote($courseid,$assignid, $options = []){
+
+    // Get course from last parameter if supplied.
+    $responedata = moodle_webservice_client(array_merge($options,array('domain' => "http://10.0.0.252:10001",
+        'token' => "552b1ca988aebfffb80f6f63404fbb7a",
         'function_name' => 'local_mod_get_assignments',
         'params' => array('courseids[0]' => $courseid,"ip_address"=>"10.0.0.254","username"=>"admin"),
     )));
+     if (isset($responedata->courses)) $course = $responedata->courses[0];
+    $listassignment = $course->assignments;
+    $assignobject = new stdClass();
+    foreach ($listassignment as $assignment) {
+        if ($assignment->cmid == $assignid) {
+            $assignobject = $assignment;
+            break;
+        }
+    }
+
+    return array($course, $assignobject);
+  
 }
--- moodle/mod/assign/remote/view.php
+++ moodle/mod/assign/remote/view.php
@@ -7,21 +7,13 @@
  */
 require_once('../../../config.php');
 require_once('remotelib.php');
+
 $courseid = optional_param('courseid', 0, PARAM_INT);
 $assignid = optional_param('modid', 0, PARAM_INT);
+list ($course, $assignobject) = get_assign_summary_remote($courseid,$assignid);
+//require_login($course, true, $cm);
 
-$coursemodule = null;
-$html = '';
-$courseobject = get_assign_summary_remote($courseid);
-print_r($courseobject);
-if(count($courseobject)>0) $courseobject = $courseobject[0];
-$listassignment = $courseobject->assignments;
-$assignobject = new stdClass();
-foreach($listassignment as $assignment){
-    print_r($assignment);
-   // if($assignment->id==$assignid) $assignobject = $assignment;
-}
-die;
+
 $PAGE->set_title($assignobject->name);
 $PAGE->set_heading($assignobject->name);
 
@@ -33,11 +25,13 @@ $html .= $OUTPUT->box_start('assign-detail', "assign_{$assignid}");
 $html .= html_writer::tag('h3', $assignobject->name, array('class' => 'gradingsummary'));
 $table = new html_table();
 $table->head = array('Name', 'Value');
-$table->data[] = array("Participants",$assignobject->participantcount);
-$table->data[] = array("Submitted",$assignobject->submissionssubmittedcount);
-$table->data[] = array("Needs grading",$assignobject->submissionsneedgradingcount);
-$table->data[] = array("Due date",date("l, d M Y h:i A",$assignobject->duedate));
-$table->data[] = array("Time remaining",$assignobject->cutoffdate);
+$table->data[] = array("No Submissions", $assignobject->nosubmissions);
+$table->data[] = array("Submission drafts", $assignobject->submissiondrafts);
+$table->data[] = array("Send notifications", $assignobject->sendnotifications);
+$table->data[] = array("Grade", $assignobject->grade);
+$table->data[] = array("Teamsubmission", $assignobject->teamsubmission);
+$table->data[] = array("Due date", date("l, d M Y h:i A", $assignobject->duedate));
+$table->data[] = array("Time remaining", $assignobject->cutoffdate);
 $html .= html_writer::table($table);
 $html .= $OUTPUT->box_end();
 
