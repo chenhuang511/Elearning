2eb9464a74311cff876c6bd7274d041b0081592c
hao.nguyen <hao.nguyen@nccsoft.vn>
2016-05-31 17:48:37 +0700

    [EL-26] add function get_lesson_timer_by_userid_and_lessonid

--- tecahub/local/lesson/externallib.php
+++ tecahub/local/lesson/externallib.php
@@ -361,4 +361,58 @@ class local_mod_lesson_external extends external_api
             )
         );
     }
+
+    public static function get_lesson_timer_by_userid_and_lessonid_parameters()
+    {
+        return new external_function_parameters(
+            array('useid' => new external_value(PARAM_INT, 'user id'),
+                'lessonid' => new external_value(PARAM_INT, 'lesson id'),
+                'options' => new external_multiple_structure (
+                    new external_single_structure(
+                        array(
+                            'name' => new external_value(PARAM_ALPHANUM,
+                                'The expected keys (value format) are:
+                                                excludemodules (bool) Do not return modules, return only the sections structure
+                                                excludecontents (bool) Do not return module contents (i.e: files inside a resource)
+                                                sectionid (int) Return only this section
+                                                sectionnumber (int) Return only this section with number (order)
+                                                cmid (int) Return only this module information (among the whole sections structure)
+                                                modname (string) Return only modules with this name "label, forum, etc..."
+                                                modid (int) Return only the module with this id (to be used with modname'),
+                            'value' => new external_value(PARAM_RAW, 'the value of the option,
+                                                                    this param is personaly validated in the external function.')
+                        )
+                    ), 'Options, used since Moodle 2.9', VALUE_DEFAULT, array())
+            )
+        );
+    }
+
+    public static function get_lesson_timer_by_userid_and_lessonid($userid, $lessonid, $options = array())
+    {
+        global $DB;
+
+        // validate params
+        $params = self::validate_parameters(self::get_lesson_timer_by_userid_and_lessonid_parameters(),
+            array(
+                'userid' => $userid,
+                'lessonid' => $lessonid,
+                'options' => $options
+            )
+        );
+
+        return $DB->get_record('lesson_timer', array('userid' => $params['userid'], 'lessonid' => $params['lessonid']), '*', MUST_EXIST);
+    }
+
+    public static function get_lesson_timer_by_userid_and_lessonid_returns() {
+        return new external_single_structure(
+            array(
+                'id' => new external_value(PARAM_INT, 'the lesson timer id'),
+                'lessonid' => new external_value(PARAM_INT, 'the lesson id', VALUE_DEFAULT),
+                'userid' => new external_value(PARAM_INT, 'the user id', VALUE_DEFAULT),
+                'starttime' => new external_value(PARAM_INT, 'start time', VALUE_DEFAULT),
+                'lessontime' => new external_value(PARAM_INT, 'lesson time', VALUE_DEFAULT),
+                'completed' => new external_value(PARAM_INT, 'completed', VALUE_DEFAULT)
+            )
+        );
+    }
 }
