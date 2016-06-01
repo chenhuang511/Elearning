0092d4184909bf12659f6f747009004e83b58348
Nguyen Tran <nguyentran@nccsoft.vn>
2016-06-01 17:25:00 +0700

    [EL-27] Remove remote folder in book

--- tecahub/mod/book/remote/view.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require(dirname(__FILE__).'/../../../config.php');
-require_once(dirname(__FILE__).'/lib.php');
-require_once(dirname(__FILE__).'/locallib.php');
-//require_once($CFG->libdir.'/completionlib.php');
-
-echo $id;
