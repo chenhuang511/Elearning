c28279779402aadf76ad1d3967dd6ab7e274da62
Minh Nguyễn <ndminh9x@gmail.com>
2016-06-02 16:39:24 +0700

    [EL-NAN]Fix a Hao

--- moodle/course/remote/locallib.php
+++ moodle/course/remote/locallib.php
@@ -101,6 +101,18 @@ function get_remote_url_content($urlid, $options = [])
     ));
 }
 
+function get_remote_course_module_by_instance($modulename, $instace, $options = array())
+{
+    return moodle_webservice_client(array_merge($options,
+        array(
+            'domain' => HUB_URL,
+            'token' => HOST_TOKEN_M,
+            'function_name' => 'core_course_get_course_module_by_instance',
+            'params' => array('module' => $modulename, 'instance' => $instace)
+        )
+    ));
+}
+
 function get_remote_context_by_instanceid_and_contextlevel($instanceid, $contextlevel, $options = array())
 {
     return moodle_webservice_client(array_merge($options,
