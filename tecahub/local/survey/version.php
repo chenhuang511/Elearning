148035904ceec5537354d2d707be5c28dbdc65a5
Tiep Phan <tieppt12@gmail.com>
2016-06-03 18:07:29 +0700

    [EL-29] rename api service.php

--- tecahub/local/survey/version.php
+++ tecahub/local/survey/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016052325;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2016052330;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016051900;        // Requires this Moodle version
 $plugin->component = 'local_survey';    // Full name of the plugin (used for diagnostics)
