d5a5682b52e8d10512262ff1865e008509f620d5
haonguyen87 <hao.nguyen@nccsoft.vn>
2016-06-06 18:07:41 +0700

    [EL-26] add style for content

--- moodle/remote/renderer.php
+++ moodle/remote/renderer.php
@@ -5,14 +5,15 @@ require_once($CFG->dirroot . '/course/renderer.php');
 function frontpage_courses($courses)
 {
     // Wrap frontpage course list in div container.
-    $content = html_writer::start_tag('div', array('id' => 'frontpage-course-list'));
-    $coursecount = 0;
+    $content = html_writer::start_tag('div', array('id' => 'frontpage-course-list', 'class' => 'container'));
 
     $chelper = new coursecat_helper();
 
+    $coursecount = 0;
     foreach ($courses as $course) {
         $coursecount++;
-        $classes = ($coursecount % 2) ? 'odd' : 'even';
+        $classes = 'coursebox col-sm-3 ';
+        $classes .= ($coursecount % 2) ? 'odd' : 'even';
         if ($coursecount == 1) {
             $classes .= ' first';
         }
@@ -20,7 +21,7 @@ function frontpage_courses($courses)
             $classes .= ' last';
         }
 
-        $classes = trim('coursebox clearfix ');
+        //$classes = trim('coursebox clearfix ');
         if ($chelper->get_show_courses() >= core_course_renderer::COURSECAT_SHOW_COURSES_EXPANDED) {
             $nametag = 'h3';
         } else {
@@ -42,7 +43,7 @@ function frontpage_courses($courses)
         // display course summary
         if (isset($course->summary) && !empty($course->summary)) {
             $content .= html_writer::start_tag('div', array('class' => 'summary'));
-            $content .= html_writer::tag('p', $course->summary);
+            $content .= html_writer::tag('p', ncc_extend_plugin::truncate($course->summary, 60));
             $content .= html_writer::end_tag('div'); // .summary
         }
 
@@ -50,4 +51,21 @@ function frontpage_courses($courses)
     }
     $content .= html_writer::end_tag('div');
     return $content;
+}
+
+class ncc_extend_plugin
+{
+    public static function truncate($text, $limit)
+    {
+        $strings = $text;
+        if (strlen($text) > $limit) {
+            $words = str_word_count($text, 2);
+            $pos = array_keys($words);
+            if (sizeof($pos) > $limit) {
+                $text = substr($text, 0, $pos[$limit]) . '...';
+            }
+            return $text;
+        }
+        return $text;
+    }
 }
