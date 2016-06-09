cc6213c0536955abc89b65718207bde5939b69d6
Nguyen Tran Nhan <nguyentran@nccsoft.vn>
2016-06-09 16:52:39 +0700

    [EL-27] revert footer

--- moodle/theme/bhxh/layout/includes/footer.php
+++ moodle/theme/bhxh/layout/includes/footer.php
@@ -18,11 +18,11 @@ $infolink = theme_bhxh_get_setting('infolink');
             <div class="container">
                 <div class="row-fluid">
                     <div class="foot-links-wrap">
-                        <div class="clearfix">
+                        <ul class="foot-links clearfix">
                             <?php
                             echo $OUTPUT->custom_menu();
                             ?>
-                        </div>
+                        </ul>
                     </div>
                 </div>
                 <div class="row-fluid">
