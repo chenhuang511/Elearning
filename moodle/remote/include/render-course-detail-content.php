1255ac9fbfd7b5a0abe9326a3f89ee12d5a22664
haonguyen87 <hao.nguyen@nccsoft.vn>
2016-06-09 10:37:30 +0700

    [EL-26] add summary of section and change tab's name

--- moodle/remote/include/render-course-detail-content.php
+++ moodle/remote/include/render-course-detail-content.php
@@ -2,17 +2,31 @@
 </div>
 
 <script>
-    (function($){
+    (function ($) {
         var summary = $('#hidden-summary'),
             content = $('#module-content'),
             summaryLink = $('#course-summary');
 
+        var sections = $('a[id^="csec-"]');
+
+        var changeContent = function (element, cnt) {
+            // remove now content
+            element.html();
+            // add new content
+            element.html(cnt);
+        }
+
+        $.each(sections, function (index, element) {
+            $(sections[index]).on('click', function () {
+                var sectionSummary = $(this).attr('data-summary');
+                changeContent(content, sectionSummary);
+            });
+        });
+
         content.html(summary.val());
 
-        summaryLink.on('click', function(){
-            content.html();
-            console.log('vao day');
-            content.html(summary.val());
+        summaryLink.on('click', function () {
+            changeContent(content, summary.val());
         });
 
     })(jQuery)
