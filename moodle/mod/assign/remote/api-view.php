a00e051dc704058cb532e126c60bfc5eacb15a1c
MinhNguyen <ndminh9x@gmail.com>
2016-06-14 18:26:08 +0700

    [EL-NaN] Change remote Asssign View

--- moodle/mod/assign/remote/api-view.php
+++ moodle/mod/assign/remote/api-view.php
@@ -4,8 +4,6 @@ require_once('../../../config.php');
 require_once('locallib.php');
 
 $cmid = optional_param('id', 0, PARAM_INT);
-$action = optional_param('action', "", PARAM_TEXT);
-
 
 // get course module
 $cm = get_remote_course_module_by_cmid('assign', $cmid);
@@ -13,20 +11,10 @@ $course = get_local_course_record($cm->course);
 
 require_login($course, false, $cm);
 
-$o = new remote_assign_mod($cm);
-$o->setOUTPUT($OUTPUT);
-$o->setPAGE($PAGE);
-$html = '';
+$context = context_module::instance($cmid);
 
-switch ($action){
-    case 'grading':
-        $html = 'do grading';
-        break;
-    case 'editsubmission':
-        $html = 'do edit';
-        break;
-    default:
-        $html = $o->view_summary();
-        break;
-}
-echo $html;
+require_capability('mod/assign:view', $context);
+
+$remoteassign = new remote_assign_mod($context, $cm, $course);
+
+echo $remoteassign->view(optional_param('action', '', PARAM_TEXT));
