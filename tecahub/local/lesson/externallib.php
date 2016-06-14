b20668222189a1754da827c1e4ea3b3a8a05b8b5
haonguyen87 <hao.nguyen@nccsoft.vn>
2016-06-14 17:15:36 +0700

    [EL-26] create api get lesson answer by id

--- tecahub/local/lesson/externallib.php
+++ tecahub/local/lesson/externallib.php
@@ -902,4 +902,64 @@ class local_mod_lesson_external extends external_api
             ), 'lesson answers'
         );
     }
+
+    public static function get_lesson_answer_by_id_parameters()
+    {
+        return new external_function_parameters(
+            array('id' => new external_value(PARAM_INT, 'the id'),
+                'options' => new external_multiple_structure (
+                    new external_single_structure(
+                        array(
+                            'name' => new external_value(PARAM_ALPHANUM,
+                                'The expected keys (value format) are:
+                                                excludemodules (bool) Do not return modules, return only the sections structure
+                                                excludecontents (bool) Do not return module contents (i.e: files inside a resource)
+                                                sectionid (int) Return only this section
+                                                sectionnumber (int) Return only this section with number (order)
+                                                cmid (int) Return only this module information (among the whole sections structure)
+                                                modname (string) Return only modules with this name "label, forum, etc..."
+                                                modid (int) Return only the module with this id (to be used with modname'),
+                            'value' => new external_value(PARAM_RAW, 'the value of the option,
+                                                                    this param is personaly validated in the external function.')
+                        )
+                    ), 'Options, used since Moodle 2.9', VALUE_DEFAULT, array())
+            )
+        );
+    }
+
+    public static function get_lesson_answer_by_id($id, $options = array())
+    {
+        global $DB;
+
+        // validate params
+        $params = self::validate_parameters(self::get_lesson_answer_by_id_parameters(),
+            array(
+                'id' => $id,
+                'options' => $options
+            )
+        );
+
+        return $DB->get_record("lesson_answers", array("id" => $params['id']));
+    }
+
+    public static function get_lesson_answer_by_id_returns()
+    {
+        return new external_single_structure(
+            array(
+                'id' => new external_value(PARAM_INT, 'id'),
+                'lessonid' => new external_value(PARAM_INT, 'lesson id'),
+                'pageid' => new external_value(PARAM_INT, 'page id'),
+                'jumpto' => new external_value(PARAM_INT, 'jumpto'),
+                'grade' => new external_value(PARAM_INT, 'grade'),
+                'score' => new external_value(PARAM_INT, 'score'),
+                'flags' => new external_value(PARAM_INT, 'flags'),
+                'timecreated' => new external_value(PARAM_INT, 'time created'),
+                'timemodified' => new external_value(PARAM_INT, 'time modified'),
+                'answer' => new external_value(PARAM_RAW, 'answer'),
+                'answerformat' => new external_value(PARAM_INT, 'answer format'),
+                'response' => new external_value(PARAM_RAW, 'response'),
+                'responseformat' => new external_value(PARAM_INT, 'response format')
+            )
+        );
+    }
 }
