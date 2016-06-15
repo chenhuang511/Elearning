ce5d5d68e53a8125d09bdb177a53e77668327420
vinh <vinh.nguyen@nccsoft.vn>
2016-06-15 14:26:26 +0700

    [EL-27]edit wiki page

--- moodle/remote/include/render-course-wiki.php
+++ moodle/remote/include/render-course-wiki.php
@@ -1,55 +1,5 @@
-<section class="discussion container" id="discussion-container">
-    <div class="discussion-body">
-        <div class="forum-nav col-sm-4">
-            <div class="forum-nav-header">
-                <div class="btn-group">
-                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span class="glyphicon glyphicon-th-list"></span>
-                        All Discussion <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                    </ul>
-                </div>
-                <form class="forum-nav-search">
-                    <div class="forum-nav-search-ff-position-fix">
-                        <label>
-                            <input class="forum-nav-search-input" type="text" placeholder="Search all posts">
-                        </label>
-                    </div>
-                </form>
-            </div>
-            <div class="forum-nav-thread-list-wrapper">
-                <div class="forum-nav-refine-bar col-sm-12">
-                    <label class="forum-nav-filter-main col-sm-6">
-                        <select class="forum-nav-filter-main-control">
-                            <option value="all">Show all</option>
-                            <option value="unread">Unread</option>
-                            <option value="unanswered">Unanswered</option>
-                        </select>
-                    </label>
-                    <label class="forum-nav-sort col-sm-6">
-                        <select class="forum-nav-sort-control">
-                            <option value="date">by recent activity</option>
-                            <option value="comments">by most activity</option>
-                            <option value="votes">by most votes</option>
-                        </select>
-                    </label>
-                </div>
-                <ul class="forum-nav-thread-list" style="height: 485px;"></ul>
-            </div>
-        </div>
-        <div class="discussion-column col-sm-8" id="discussion-column">
-            <div class="forum-content">
-                <div class="discussion-article view-discussion-home">
-                    <section class="home-header">
-                        <h3 class="home-title">Quản trị dự án Công nghệ thông tin</h3>
-                    </section>
-                </div>
-            </div>
-        </div>
+<section class="container-course-wiki" >
+    <div class="wiki-wrapper">
+
     </div>
 </section>
