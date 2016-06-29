1d3fe2dbb9870d594277558442ca84aeabbd9f27
vinh <vinh.nguyen@nccsoft.vn>
2016-06-29 18:21:14 +0700

    [EL-27] update logic page wiki

--- moodle/lib/additionallib.php
+++ moodle/lib/additionallib.php
@@ -35,7 +35,10 @@ require_once($CFG->dirroot . '/fdefine.php');
 function get_course_id_by_remote_id($remotecourseid) {
     global $DB;
 
-    $retval = $DB->get_field('course', 'id', array('remoteid' => $remotecourseid));
+    $retval = $remotecourseid;
+    if (MOODLE_RUN_MODE === MOODLE_MODE_HUB && $remotecourseid != SITEID) {
+        $retval = $DB->get_field('course', 'id', array('remoteid' => $remotecourseid));
+    }
 
     return $retval;
 }
