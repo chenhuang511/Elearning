d0754d4e337860b63b081463892bcd63b48fa03c
Nguyen Tran Nhan <nguyentran@nccsoft.vn>
2016-07-01 18:18:00 +0700

    [EL-27] Fix missing file

---
+++ moodle/mod/questionnaire/remote/locallib.php
@@ -0,0 +1,26 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vanhaIT
+ * Date: 30/05/2016
+ * Time: 3:50 CH
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+require_once($CFG->dirroot . '/lib/remote/lib.php');
+require_once($CFG->dirroot . '/lib/additionallib.php');
+
+
+function get_remote_questionnaire_by_id($id) {
+    $resp = moodle_webservice_client(
+        array(
+            'domain' => HUB_URL,
+            'token' => HOST_TOKEN,
+            'function_name' => 'local_mod_bigbluebuttonbn_get_bigbluebuttonbn_by_id',
+            'params' => array('id' => $id)
+        )
+    );
+    return $resp;
+}
