6f4bdd68728289ca2cd2a7cf98771c5d7e06ac90
thienasz <hung.le@stash.nccsoft.vn>
2016-07-13 13:59:22 +0700

    create api for certificate

---
+++ tecahub/local/certificate/version.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * 
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2016071303;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2016051900;        // Requires this Moodle version
+$plugin->component = 'local_certificate'; 			// Full name of the plugin (used for diagnostics)
