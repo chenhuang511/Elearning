258f63b8e1b5a12299f4d0c51a875858b1132fe5
Nguyen Tran Nhan <nguyentran@nccsoft.vn>
2016-07-14 11:17:24 +0700

    [EL-27] FIX resource issue

--- moodle/remote/include/render-course-detail-menu.php
+++ moodle/remote/include/render-course-detail-menu.php
@@ -62,7 +62,7 @@
                                     <?php
                                 } else { ?>
                                     <a class="sublink get-remote-content"
-                                       data-module='<?php echo json_encode(array('url' => $CFG->wwwroot . '/mod/' . $module->modname . '/remote/view.php', 'params' => array('id' => $module->id), 'method' => 'get')); ?>'
+                                       data-module='<?php echo json_encode(array('url' => $CFG->wwwroot . '/mod/' . $module->modname . '/remote/view.php', 'params' => array('id' => $module->id, 'nonajax' => false), 'method' => 'get')); ?>'
                                        href="#">
                                         <span class="icon-bxh icon-<?php echo $module->modname; ?>" ></span><?php echo $module->name; ?>
                                     </a>
