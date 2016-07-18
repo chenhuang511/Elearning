e493662a19ee8a9309834e1880cd57868bfe48d2
Tiep Phan <tieppt12@gmail.com>
2016-07-18 14:23:17 +0700

    [EL-29] update sql command for assign table

--- moodle/update.sql
+++ moodle/update.sql
@@ -3,5 +3,6 @@ alter table mdl_course add hostid smallint(4) not null default 0;
 alter table mdl_course add categoryname varchar(255) NOT NULL DEFAULT '';
 alter table mdl_course_categories add remoteid bigint(10) not null;
 alter table mdl_course_categories add hostid smallint(4) not null default 0;
+alter table mdl_assign add remoteid BIGINT(10) NOT NULL;
 
 drop table mdl_mnetservice_enrol_courses;
