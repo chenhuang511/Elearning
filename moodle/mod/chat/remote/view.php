32b66914716c772b43005442ba0362f434aa5854
vinh <vinh.nguyen@nccsoft.vn>
2016-07-20 09:25:58 +0700

    [EL-27] delete vardump

--- moodle/mod/chat/remote/view.php
+++ moodle/mod/chat/remote/view.php
@@ -178,7 +178,6 @@ global $USER;
 $userid = get_remote_mapping_user($USER->id)[0]->id;
 $chatsid = get_remote_chat_login_user($userid, $chat->id, $groupid = 0);
 if ($chatusers = chat_get_users($chat->id, $currentgroup, $cm->groupingid, $chatsid)) {
-    var_dump($chatusers);
     $timenow = time();
     echo $OUTPUT->box_start('generalbox', 'chatcurrentusers');
     echo $OUTPUT->heading($strcurrentusers, 3);
