878339c627915deda2b1961f519273cb4fc153e3
haonguyen87 <hao.nguyen@nccsoft.vn>
2016-08-05 17:42:00 +0700

    [MOODLE-100] restore bhxh theme and sync stylesheet

---
+++ moodle/theme/bhxh/layout/includes/footer.php
@@ -0,0 +1,47 @@
+<?php
+$footnote = theme_bhxh_get_setting('footnote', 'format_html');
+
+$fburl = theme_bhxh_get_setting('fburl');
+$pinurl = theme_bhxh_get_setting('pinurl');
+$twurl = theme_bhxh_get_setting('twurl');
+$gpurl = theme_bhxh_get_setting('gpurl');
+
+$address = theme_bhxh_get_setting('address');
+$emailid = theme_bhxh_get_setting('emailid');
+$phoneno = theme_bhxh_get_setting('phoneno');
+$copyright_footer = theme_bhxh_get_setting('copyright_footer');
+
+?>
+    <footer id="footer">
+        <div class="footer-main">
+            <div class="container">
+                <div class="row-fluid">
+                    <div class="foot-links-wrap">
+                        <ul class="foot-links clearfix">
+                            <?php
+                            echo $OUTPUT->custom_menu();
+                            ?>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row-fluid">
+                    <div class="infoarea">
+                        <div class="logo-footer logo">
+                            <a href="<?php echo $CFG->wwwroot; ?>">
+                                <div class="logo-section">
+                                    <img src="<?php echo get_logo_url(); ?>" class="site-logo-img" alt="bhxh">
+                                </div>
+                                <div class="logo-section">
+                                    <div class="logo-title">
+                                        <?php echo get_string('configtitle', 'theme_bhxh'); ?>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--E.O.Footer-->
+<?php echo $OUTPUT->standard_end_of_body_html() ?>
---
+++ moodle/theme/bhxh/layout/includes/header.php
@@ -0,0 +1,60 @@
+<?php global $CFG; ?>
+<header id="header">
+    <div class="header-main">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-8">
+                    <div class="logo">
+                        <a href="<?php echo $CFG->wwwroot; ?>">
+                            <div class="logo-section">
+                                <img src="<?php echo get_logo_url(); ?>" class="site-logo-img" alt="bhxh">
+                            </div>
+                            <div class="logo-section">
+                                <div class="logo-title">
+                                    <?php echo get_string('configtitle', 'theme_bhxh'); ?>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="navbar">
+                        <div class="navbar-inner">
+                            <div class="clearfix">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-responsive-collapse" aria-expanded="false">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <?php if ($CFG->branch > "27"): ?>
+                                    <?php echo $OUTPUT->user_menu(); ?>
+                                <?php endif; ?>
+                            </div>
+                            <div class="nav-collapse collapse navbar-responsive-collapse clearfix">
+                                <?php echo $OUTPUT->custom_menu(); ?>
+                                <ul class="nav pull-right">
+                                    <li><?php echo $OUTPUT->page_heading_menu(); ?></li>
+                                    <?php if ($CFG->branch < "28"): ?>
+                                        <li class="navbar-text"><?php echo $OUTPUT->login_info() ?></li>
+                                    <?php endif; ?>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php if (isloggedin() && !isguestuser()): ?>
+        <?php else: ?>
+            <div class="banner">
+                <div class="banner-content css">
+                    <div class="font d1">Chào mừng đến với</div>
+                    <div class="font d2">TRƯỜNG ĐÀO TẠO NGHIỆP VỤ</div>
+                    <div class="font d3">BẢO HIỂM XÃ HỘI VIỆT NAM</div>
+                </div>
+            </div>
+    <?php endif; ?>
+</header>
+<!--E.O.Header-->
