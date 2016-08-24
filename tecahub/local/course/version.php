b2318c4d8c3e2def1de6000df672125bedba4362
haonguyen87 <hao.nguyen@nccsoft.vn>
2016-08-24 14:34:27 +0700

    [MOODLE-110] create api save a new mdl course

--- tecahub/local/course/version.php
+++ tecahub/local/course/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016052346;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2016052347;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016051900;        // Requires this Moodle version
 $plugin->component = 'local_course'; 			// Full name of the plugin (used for diagnostics)
