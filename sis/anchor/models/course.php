e8b28377c94319783662857a26db7dbbe25d94fa
thienasz <thien.dang@nccsoft.vn>
2016-09-26 18:32:40 +0700

    [MOODLE-137]Add list course example

---
+++ sis/anchor/models/course.php
@@ -0,0 +1,41 @@
+<?php
+
+class Course extends Base {
+
+    public static $table = 'courses';
+
+    public static function get_courses_by_user($id, $page = 1, $per_page = 10) {
+        // get total
+        $query = static::join(Base::table('user_course'), Base::table('courses.id'), '=', Base::table('user_course.courseid'))
+            ->join(Base::table('users'), Base::table('users.id'), '=', Base::table('user_course.userid'))
+            ->where(Base::table('users.id'), '=', $id);
+
+        $total = $query->count();
+
+        // get posts
+        $posts = $query->take($per_page)
+            ->skip(--$page * $per_page)
+            ->get(array(Base::table('courses.*'),
+                Base::table('courses.id as courseid')));
+
+        return array($total, $posts);
+    }
+
+    public static function get_grade_by_course($courseid, $page = 1, $per_page = 10) {
+        // get total
+        $query = static::join(Base::table('student_course'), Base::table('courses.id'), '=', Base::table('student_course.courseid'))
+            ->join(Base::table('students'), Base::table('students.id'), '=', Base::table('student_course.studentid'))
+            ->where(Base::table('courses.id'), '=', $courseid);
+
+        $total = $query->count();
+
+        // get posts
+        $posts = $query->take($per_page)
+            ->skip(--$page * $per_page)
+            ->get(array(Base::table('students.*'),
+                Base::table('student_course.grade'),
+                Base::table('courses.id as courseid')));
+
+        return array($total, $posts);
+    }
+}
--- sis/anchor/routes/grades.php
+++ sis/anchor/routes/grades.php
@@ -3,20 +3,53 @@
 Route::collection(array('before' => 'auth,csrf'), function() {
 
     Route::get(array('admin/courses', 'admin/courses/(:num)'), function($page = 1) {
-        $perpage = Config::get('admin.posts_per_page');
-        $total = Page::where(Base::table('pages.parent'), '=', '0')->count();
-        $pages = Page::sort('title')->where(Base::table('pages.parent'), '=', '0')->take($perpage)->skip(($page - 1) * $perpage)->get();
-        $url = Uri::to('admin/pages');
+
+        // get public listings
+        $userid = 1; // hardcode for example
+        list($total, $pages) = Course::get_courses_by_user($userid, $page, $perpage = Config::get('admin.posts_per_page'));
+
+        $url = Uri::to('admin/courses');
 
         $pagination = new Paginator($pages, $total, $page, $perpage, $url);
 
         $vars['messages'] = Notify::read();
         $vars['pages'] = $pagination;
-        $vars['status'] = 'all';
         //need process here
         return View::create('grades/courses', $vars)
             ->partial('header', 'partials/header')
             ->partial('footer', 'partials/footer');
     });
 
+    Route::get(array('admin/courses/info/(:num)'), function($id = 1) {
+
+        // get public listings
+        $pages = Course::where('id', '=', $id)->get()[0];
+
+        $url = Uri::to('admin/courses');
+        var_dump($pages);
+        $vars['messages'] = Notify::read();
+        $vars['pages'] = $pages;
+        //need process here
+        return View::create('grades/courseinfo', $vars)
+            ->partial('header', 'partials/header')
+            ->partial('footer', 'partials/footer');
+    });
+
+    Route::get(array('admin/grades/(:num)', 'admin/grades/(:num)/(:num)'), function($courseid, $page = 1) {
+
+        // get public listings
+        list($total, $pages) = Course::get_grade_by_course($courseid, $page, $perpage = Config::get('admin.posts_per_page'));
+
+        $url = Uri::to('admin/grades/' . $courseid);
+
+        $pagination = new Paginator($pages, $total, $page, $perpage, $url);
+
+        $vars['messages'] = Notify::read();
+        $vars['pages'] = $pagination;
+        $vars['courseid'] = $courseid;
+        //need process here
+        return View::create('grades/grades', $vars)
+            ->partial('header', 'partials/header')
+            ->partial('footer', 'partials/footer');
+    });
 });
