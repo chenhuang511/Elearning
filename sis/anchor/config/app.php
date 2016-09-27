c0c31180c8511789615bc452be1a779bbca99f4f
thienasz <thien.dang@nccsoft.vn>
2016-09-27 09:18:03 +0700

    add config for anchor

---
+++ sis/anchor/config/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'url' => '/',
+	'index' => '',
+	'timezone' => 'Asia/Ho_Chi_Minh',
+	'key' => 'r1Zk7vjfWMSLD5fUW2BWPVPRN872AJip',
+	'language' => 'en_GB',
+	'encoding' => 'UTF-8'
+);
