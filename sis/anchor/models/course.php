3b1700b01dd9e0ae7184df9b06129f4212f814e1
haonguyen87 <hao.nguyen@nccsoft.vn>
2016-09-28 18:14:28 +0700

    [MOODLE-126] create view cours

--- sis/anchor/models/course.php
+++ sis/anchor/models/course.php
@@ -10,28 +10,44 @@ class Course extends Base
         if ($userid != null) {
             return self::getCoursesByUserId($userid, $page, $perpage);
         }
+
+        return self::getCoursesBySchoolId($page, $perpage);
     }
 
-    private static function getCoursesBySchool($page = 1, $perpage = 10) {
+    private static function getCoursesBySchoolId($page = 1, $perpage = 10)
+    {
+        $query = static::join(Base::table('user_course'), Base::table('courses.id'), '=', Base::table('user_course.courseid'))
+            ->join(Base::table('users'), Base::table('users.id'), '=', Base::table('user_course.userid'));
 
+        $total = $query->count();
+
+        // get courses
+        $courses = $query->take($perpage)
+            ->skip(--$page * $perpage)
+            ->get(array(
+                Base::table('courses.*'),
+                Base::table('users.username')
+            ));
+
+        return array($total, $courses);
     }
 
-    private static function getCoursesByUserId($id, $page = 1, $per_page = 10)
+    private static function getCoursesByUserId($userid, $page = 1, $per_page = 10)
     {
         // get total
         $query = static::join(Base::table('user_course'), Base::table('courses.id'), '=', Base::table('user_course.courseid'))
             ->join(Base::table('users'), Base::table('users.id'), '=', Base::table('user_course.userid'))
-            ->where(Base::table('users.id'), '=', $id);
+            ->where(Base::table('users.id'), '=', $userid);
 
         $total = $query->count();
 
-        // get posts
-        $posts = $query->take($per_page)
+        // get courses
+        $courses = $query->take($per_page)
             ->skip(--$page * $per_page)
             ->get(array(Base::table('courses.*'),
                 Base::table('courses.id as courseid')));
 
-        return array($total, $posts);
+        return array($total, $courses);
     }
 
     public static function get_grade_by_course($courseid, $page = 1, $per_page = 10)
