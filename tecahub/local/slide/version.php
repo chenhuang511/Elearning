68261f052bb2cb1cebe234cc2f96f19094c1edac
Tiep Phan <tieppt12@gmail.com>
2016-09-29 18:22:07 +0700

    [EL-29] change api local name

--- tecahub/local/slide/version.php
+++ tecahub/local/slide/version.php
@@ -26,4 +26,4 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016052335;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016051900;        // Requires this Moodle version
-$plugin->component = 'local_url';    // Full name of the plugin (used for diagnostics)
+$plugin->component = 'local_slide';    // Full name of the plugin (used for diagnostics)
