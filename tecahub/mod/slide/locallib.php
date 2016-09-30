67c9da787e1f74e4e6aba14876a4d492fbf1a246
Tiep Phan <tieppt12@gmail.com>
2016-09-30 14:58:21 +0700

    [EL-29] change function cb

--- tecahub/mod/slide/locallib.php
+++ tecahub/mod/slide/locallib.php
@@ -91,7 +91,7 @@ function slide_get_full_url($slide, $cm, $course, $config=null) {
     if (preg_match('/^(\/|https?:|ftp:)/i', $fullurl) or preg_match('|^/|', $fullurl)) {
         // encode extra chars in URLs - this does not make it always valid, but it helps with some UTF-8 problems
         $allowed = "a-zA-Z0-9".preg_quote(';/?:@=&$_.+!*(),-#%', '/');
-        $fullurl = preg_replace_callback("/[^$allowed]/", 'url_filter_callback', $fullurl);
+        $fullurl = preg_replace_callback("/[^$allowed]/", 'slide_filter_callback', $fullurl);
     } else {
         // encode special chars only
         $fullurl = str_replace('"', '%22', $fullurl);
