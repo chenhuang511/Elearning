b6682ca31a355aa1c69daaf977e2527d0cbd0340
nhimlicy <thien.dang@nccsoft.vn>
2016-10-05 12:09:14 +0700

    advance in course

--- sis/anchor/views/advance/add.php
+++ sis/anchor/views/advance/add.php
@@ -4,7 +4,7 @@
     <h1><?php echo __('advance.advance'); ?></h1>
 </hgroup>
 
-<form method="post" action="<?php echo Uri::to('admin/advance/add'); ?>" enctype="multipart/form-data" novalidate>
+<form method="post" action="<?php echo Uri::to('admin/advance/course/'.$course_id.'/add'); ?>" enctype="multipart/form-data" novalidate>
 
     <input name="token" type="hidden" value="<?php echo $token; ?>">
 
@@ -19,7 +19,7 @@
                     'data-loading' => __('global.saving')
                 )); ?>
 
-                <?php echo Html::link('admin/advance' , __('global.cancel'), array(
+                <?php echo Html::link('admin/advance/course/'.$course_id, __('global.cancel'), array(
                     'class' => 'btn cancel blue'
                 )); ?>
             </aside>
@@ -32,7 +32,7 @@
             <table class="form_advance">
                 <tr>
                     <td> <label for="label-slug"><?php echo __('advance.courses'); ?>:</label></td>
-                    <td><?php echo Form::select('course_id', $course, Input::previous('applicant_id'), array('id' => 'label-course_id')); ?></td>
+                    <td> <label for="label-slug" style="padding-left: 0px !important;"><?php echo $course; ?></label></td>
                 </tr>
                 <tr>
                     <td> <label for="label-slug"><?php echo __('advance.applicant'); ?>:</label></td>
