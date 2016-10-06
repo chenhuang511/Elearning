ee2f27a55e9788159e507258d9b9faf86573e5df
Quan <quan.nguyen>
2016-10-06 10:03:49 +0700

    [MOODLE-139] edit api

--- moodle/local/user/db/services.php
+++ moodle/local/user/db/services.php
@@ -2,9 +2,9 @@
 //var_dump('user services.php');
 $functions = array(
     'local_host_get_user_link_profile' => array(
-        'classname'     => 'local_certificate_external',
+        'classname'     => 'local_user_external',
         'methodname'    => 'get_user_link_profile',
-        'classpath'     => 'local/certificate/externallib.php',
+        'classpath'     => 'local/user/externallib.php',
         'description'   => 'Get id user by remote id',
         'type'		    => 'read',
         'ajax'		    => true
