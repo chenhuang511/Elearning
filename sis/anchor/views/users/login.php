4984a1e5e60cff6907e8d7db44d5f3c0f49331e1
haonguyen87 <hao.nguyen@nccsoft.vn>
2016-10-07 10:38:31 +0700

    [MOODLE-141] change UI for login page

--- sis/anchor/views/users/login.php
+++ sis/anchor/views/users/login.php
@@ -1,59 +1,49 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title><?php echo __('global.manage'); ?> <?php echo Config::meta('sitename'); ?></title>
-	<link rel="shortcut icon" type="image/png" href="<?php echo asset('anchor/views/assets/img/favicon.png'); ?>" />
+    <meta charset="utf-8">
+    <title><?php echo __('global.manage'); ?><?php echo Config::meta('sitename'); ?></title>
+    <link rel="shortcut icon" type="image/png" href="<?php echo asset('anchor/views/assets/img/favicon.png'); ?>"/>
+    <link rel="stylesheet" href="<?php echo asset('anchor/views/assets/css/login.css'); ?>">
+    <link rel="stylesheet" href="<?php echo asset('anchor/views/assets/css/font-awesome.min.css'); ?>">
 
-	<script src="<?php echo asset('anchor/views/assets/js/zepto.js'); ?>"></script>
-
-	<link rel="stylesheet" href="<?php echo theme_url('/css/bootstrap.min.css'); ?>">
-
-	<link rel="stylesheet" href="<?php echo asset('anchor/views/assets/css/reset.css'); ?>">
-	<link rel="stylesheet" href="<?php echo asset('anchor/views/assets/css/admin.css'); ?>">
-	<link rel="stylesheet" href="<?php echo asset('anchor/views/assets/css/login.css'); ?>">
-	<link rel="stylesheet" href="<?php echo asset('anchor/views/assets/css/notifications.css'); ?>">
-	<link rel="stylesheet" href="<?php echo asset('anchor/views/assets/css/forms.css'); ?>">
-	<link rel="stylesheet" href="<?php echo asset('anchor/views/assets/css/font-awesome.min.css'); ?>">
-
-	<link rel="stylesheet" href="<?php echo theme_url('/css/bootstrap.min.css'); ?>">
-	<link rel="stylesheet" href="<?php echo theme_url('/css/bhxh.css'); ?>">
-
-	<link rel="stylesheet" media="(max-width: 980px), (max-device-width: 480px)" href="<?php echo asset('anchor/views/assets/css/small.css'); ?>">
-	<script src="<?php echo theme_url('/js/jquery-3.1.0.min.js'); ?>"></script>
-	<meta http-equiv="X-UA-Compatible" content="chrome=1">
-	<meta name="viewport" content="width=600">
+    <link rel="stylesheet" media="(max-width: 980px), (max-device-width: 480px)"
+          href="<?php echo asset('anchor/views/assets/css/small.css'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <meta name="viewport" content="width=600">
 </head>
-<body class="<?php echo Auth::guest() ? 'login' : 'admin'; ?> <?php echo str_replace('_','-',Config::app('language')); ?>">
-<section class="login login-wrapper">
-	<div class="login-content">
-		<?php echo $messages; ?>
-		<?php $user = filter_var(Input::previous('user'), FILTER_SANITIZE_STRING); ?>
-		<form method="post" action="<?php echo Uri::to('admin/login'); ?>">
-
-			<input name="token" type="hidden" value="<?php echo $token; ?>">
-
-			<fieldset>
-				<p><label for="label-user"><?php echo __('users.username'); ?>:</label>
-					<?php echo Form::text('user', $user, array(
-						'id' => 'label-user',
-						'autocapitalize' => 'off',
-						'autofocus' => 'true',
-						'placeholder' => __('users.username')
-					)); ?></p>
-
-				<p><label for="label-pass"><?php echo __('users.password'); ?>:</label>
-					<?php echo Form::password('pass', array(
-						'id' => 'pass',
-						'placeholder' => __('users.password'),
-						'autocomplete' => 'off'
-					)); ?></p>
-
-				<p class="buttons"><a href="<?php echo Uri::to('admin/amnesia'); ?>"><?php echo __('users.forgotten_password'); ?></a>
-					<button type="submit"><?php echo __('global.login'); ?></button></p>
-			</fieldset>
-		</form>
-	</div>
-</section>
+<body
+    class="<?php echo Auth::guest() ? 'login' : 'admin'; ?> <?php echo str_replace('_', '-', Config::app('language')); ?>">
+<div class="pen-title">
+    <h1>Hệ thống quản lý đào tạo</h1>
+</div>
+<div class="module form-module">
+    <div class="toggle"><i class="fa fa-times fa-pencil"></i>
+    </div>
+    <div class="form">
+        <h2>Đăng nhập hệ thống</h2>
+        <?php echo $messages; ?>
+        <?php $user = filter_var(Input::previous('user'), FILTER_SANITIZE_STRING); ?>
+        <form method="post" action="<?php echo Uri::to('admin/login'); ?>">
+            <input name="token" type="hidden" value="<?php echo $token; ?>">
+            <?php echo Form::text('user', $user, array(
+                'id' => 'label-user',
+                'autocapitalize' => 'off',
+                'autofocus' => 'true',
+                'placeholder' => __('users.username')
+            )); ?>
+            <?php echo Form::password('pass', array(
+                'id' => 'pass',
+                'placeholder' => __('users.password'),
+                'autocomplete' => 'off'
+            )); ?>
+            <button type="submit"><?php echo __('global.login'); ?></button>
+        </form>
+    </div>
+    <div class="cta"><a
+            href="<?php echo Uri::to('admin/amnesia'); ?>"><?php echo __('users.forgotten_password'); ?></a></div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="<?php echo asset_url('js/bootstrap.min.js'); ?>"></script>
 </body>
 </html>
